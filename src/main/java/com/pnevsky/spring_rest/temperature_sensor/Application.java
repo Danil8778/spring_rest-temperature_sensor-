77036bc3c4f6f294a0917221d070be61207ea072
Danil Pnevsky <dpnevsky@gmail.com>
2023-05-16 23:16:53 +0300

    initialization of the temperature_sensor project

---
+++ src/main/java/com/pnevsky/spring_rest/temperature_sensor/Application.java
@@ -0,0 +1,13 @@
+package com.pnevsky.spring_rest.temperature_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnevsky/spring_rest/temperature_sensor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnevsky.spring_rest.temperature_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
